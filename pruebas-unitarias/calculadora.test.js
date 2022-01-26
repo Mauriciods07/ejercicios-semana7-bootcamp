378887f658a0c989eb9aa671a6b0e1770f5c333e
Mauricio Díaz <mauricio.idiazs@gmail.com>
2022-01-26 10:20:34 -0600

    Ejercicios

--- pruebas-unitarias/calculadora.test.js
+++ pruebas-unitarias/calculadora.test.js
@@ -3,7 +3,7 @@
     si sale una importación extra, hay que borrarla 
     se debe encerrar la función entre llaves
 */
-const { sumar } = require('./calculadora');
+const { sumar, restar, multiplicar, dividir } = require('./calculadora');
 
 /* Esto sólo es una descripción, si encuentra errores, no te dice dónde está */
 describe('Pruebas en el componente calculadora', () => {
@@ -12,6 +12,19 @@ describe('Pruebas en el componente calculadora', () => {
         /* resultado esperado */
         expect(sumar(3, 5)).toBe(8);
     });
+
+    test('Prueba en la función restar', () => {
+        expect(restar(5, 3)).toBe(2);
+    });
+
+    test('Prueba en la función multiplicar', () => {
+        expect(multiplicar(5, 3)).toBe(15);
+    });
+
+    test('Prueba en la función dividir', () => {
+        expect(dividir(6, 3)).toBe(2);
+    });
+
 })
 
 /* 
